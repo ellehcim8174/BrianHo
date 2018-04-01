68f60426f37b91bbceaf2815cad72c2844ca010d
ellehcim8174 <michelle.mak.8174@gmail.com>
2018-03-31 19:04:47 -0700

    decoder_2motors

--- reading_count_arduino/reading_count_arduino.ino
+++ reading_count_arduino/reading_count_arduino.ino
@@ -1,51 +1,88 @@
 #include "Arduino.h"
 
-//motor driver pins 
-#define ENABLE    5   //PWM pin
-#define dir1      6
-#define dir2      7
-//encoder reading variables
-double  count;
-int     SEL1 = 11;
-//int   SEL2 = 12;    //SEL2 always low to read 3rd byte and LSB, keep pin GND
-int     OE = 13;
-byte    result_high;
-byte    result_low;
-byte    high_new;
-byte    high_old;
-byte    low_new;
-byte    low_old;
-byte    byte_data;
+//Brian motor driver pins 
+#define ENABLE_BR    5   //PWM pin
+#define dir1_BR      7
+#define dir2_BR      6
+//Ho motor driver pins
+#define ENABLE_HO    2
+#define dir1_HO      3
+#define dir2_HO      4
+
+//Brian encoder select/OE pins
+double  count_BR = 0;
+int     SEL1_BR = 11;
+//int   SEL2_BR = 12;    //SEL2 always low to read 3rd byte and LSB, keep pin GND
+int     OE_BR = 13;
+//Ho encoder select/OE pins
+double  count_HO = 0;
+int     SEL1_HO = 8;
+//int   SEL2_HO = 9;
+int     OE_HO = 10;
+
+//bit reading variables
+byte    result_high_BR;
+byte    result_low_BR;
+byte    result_high_HO;
+byte    result_low_HO;
+byte    high_new_BR;
+byte    high_old_BR;
+byte    high_new_HO;
+byte    high_old_HO;
+byte    low_new_BR;
+byte    low_old_BR;
+byte    low_new_HO;
+byte    low_old_HO;
+byte    byte_data_BR;
+byte    byte_data_HO;
 
 //runMotor, PID variables
-double  desired_location = 80;             // step response input
-int     current_location;                  // feedback
-double  error;                             // error after subracting negative feedback
-double  PID_value;                         // PID value for input
-long    raw_PWM;                           // signed PWM
-int     PWM_mag;                           // just the magnitude
-float   error_decimal;                     // for adjusting gain of input to system
+int  desired_location_HO = 0;           // step response input
+int  desired_location_BR = 0;           // step response input
+double  error_HO;                          // error after subracting negative feedback
+double  error_BR;
+double  PID_value_HO;                      // PID value for input
+double  PID_value_BR;                      // PID value for input
+long    raw_PWM_HO;
+long    raw_PWM_BR;                        // signed PWM
+float   error_decimal_HO;                  // for adjusting gain of input to system
+float   error_decimal_BR;                  // for adjusting gain of input to system
 
-float   Kp = 0.9;                          // Proportional gain, 0.211 (up to 0.9)
-float   Ki = 0.170;                        // Integrator gain, 0.170
-float   Kd = 0.058;                        // Derivative gain, 0.058
+float   Kp_HO = 2.058;                     // Proportional gain, 0.211 (up to 0.9)
+float   Ki_HO = 9.354;                     // Integrator gain, 0.170
+float   Kd_HO = 0.111;                     // Derivative gain, 0.058
 
-/*float Kp = 1;                            // only P
-float   Ki = 0;               
-float   Kd = 0;  */
+float   Kp_BR = 0.211;                       // Proportional gain, 0.211 (up to 0.9)
+float   Ki_BR = 0.170;                     // Integrator gain, 0.170
+float   Kd_BR = 0.058;                     // Derivative gain, 0.058
 
-double  integral = 0;                      // Integrator term
-double  derivative = 0;                    // Derivative term 
-double  last_error = 0;                    // Used for saving last error to calculate derivative
-long    last_micros_integral = 0;          // microseconds since last loop of motor.ino integrator calculation
-long    last_micros_derivative = 0;        // microseconds sine last loop of motor.ino derivative calculation
-int     derivative_counter = 0;            // sets a delay so we can get somewhat of a real derivative
+/*float   Kp_HO = 1;                     // ONLY FEEDBACK
+float   Ki_HO = 0;                     
+float   Kd_HO = 0;                     
+
+float   Kp_BR = 1;                       
+float   Ki_BR = 0;                     
+float   Kd_BR = 0;                          */
+
+
+double  integral_HO = 0;                      // Integrator term
+double  integral_BR = 0;                      // Integrator term
+double  derivative_HO = 0;                    // Derivative term 
+double  derivative_BR = 0;                    // Derivative term 
+double  last_error_HO = 0;                    // Used for saving last error to calculate derivative
+double  last_error_BR = 0;                    // Used for saving last error to calculate derivative
+long    last_micros_integral_HO = 0;          // microseconds since last loop of motor.ino integrator calculation
+long    last_micros_integral_BR = 0;          // microseconds since last loop of motor.ino integrator calculation
+long    last_micros_derivative_HO = 0;        // microseconds sine last loop of motor.ino derivative calculation
+long    last_micros_derivative_BR = 0;        // microseconds sine last loop of motor.ino derivative calculation
+int     derivative_counter_HO = 0;            // sets a delay so we can get somewhat of a real derivative
+int     derivative_counter_BR = 0;            // sets a delay so we can get somewhat of a real derivative
 
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
-  //pins for reading decoder
+  //pin declaration for Brian Motor
   pinMode(30, INPUT);
   pinMode(31, INPUT); 
   pinMode(32, INPUT);
@@ -54,76 +91,133 @@ void setup() {
   pinMode(35, INPUT); 
   pinMode(36, INPUT);
   pinMode(37, INPUT);
-  pinMode(SEL1, OUTPUT);
+  pinMode(SEL1_BR, OUTPUT);
   //pinMode(SEL2, OUTPUT);
-  pinMode(OE, OUTPUT);
+  pinMode(OE_BR, OUTPUT);
+  pinMode(ENABLE_BR, OUTPUT);
+  pinMode(dir1_BR, OUTPUT);
+  pinMode(dir2_BR, OUTPUT);
 
-  //pins for bottom motor
-  pinMode(ENABLE, OUTPUT);
-  pinMode(dir1, OUTPUT);
-  pinMode(dir2, OUTPUT);
+  //pin declaration for Ho Motor
+  pinMode(40, INPUT);
+  pinMode(41, INPUT); 
+  pinMode(42, INPUT);
+  pinMode(43, INPUT); 
+  pinMode(44, INPUT);
+  pinMode(45, INPUT); 
+  pinMode(46, INPUT);
+  pinMode(47, INPUT);
+  pinMode(SEL1_HO, OUTPUT);
+  //pinMode(SEL2_HO, OUTPUT);
+  pinMode(OE_HO, OUTPUT);
+  pinMode(ENABLE_HO, OUTPUT);
+  pinMode(dir1_HO, OUTPUT);
+  pinMode(dir2_HO, OUTPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
   //Input to decoder chip to select upper byte 
-  digitalWrite(OE, LOW);
-  digitalWrite(SEL1, LOW);
-  //digitalWrite(SEL2, LOW);
-  get_high();
+  digitalWrite(OE_BR, LOW);
+  digitalWrite(SEL1_BR, LOW);
+  //digitalWrite(SEL2_BR, LOW);
+  get_high_BR();
+  digitalWrite(OE_HO, LOW);
+  digitalWrite(SEL1_HO, LOW);
+  //digitalWrite(SEL2_HO, LOW);
+  get_high_HO();
 
   //Input to chip to select lower byte
-  digitalWrite(SEL1, HIGH);
-  //digitalWrite(SEL2, LOW);
-  get_low();
+  digitalWrite(SEL1_BR, HIGH);
+  //digitalWrite(SEL2_BR, LOW);
+  get_low_BR();
+  digitalWrite(SEL1_HO, HIGH);
+  //digitalWrite(SEL2_HO, LOW);
+  get_low_HO();
  
-  digitalWrite(OE, HIGH);
+  digitalWrite(OE_BR, HIGH);
+  digitalWrite(OE_HO, HIGH);
 
   //run motor and get count readings
   runMotor();
   //data_manipulations
-  count = (result_high<<8) | result_low;   //concatenating upper byte and lower byte to 16 bits
-  count = double(count);
-  Serial.println(count);
+  count_BR = (result_high_BR<<8) | result_low_BR;   //concatenating upper byte and lower byte to 16 bits - BRIAN
+  count_BR = double(count_BR);
+  count_HO = (result_high_HO<<8) | result_low_HO;   //concatenating upper byte and lower byte to 16 bits - HO
+  count_HO = double(count_HO);
+  //Serial.print(count_BR);
+  //Serial.print("\t");
+  //Serial.println(count_HO);
 }
 
 //function for getting 2nd LSB
-void get_high (){
-  high_new = get_byte();
-  high_old = get_byte();
+void get_high_BR (){
+  high_new_BR = get_byte_BR();
+  high_old_BR = get_byte_BR();
 
-  while (!(high_new == high_old)){
-    high_new = get_byte();
-    high_old = get_byte();
+  while (!(high_new_BR == high_old_BR)){
+    high_new_BR = get_byte_BR();
+    high_old_BR = get_byte_BR();
     }
-  result_high = high_new ;
+  result_high_BR = high_new_BR ;
+  return;
+}
+void get_high_HO (){
+  high_new_HO = get_byte_HO();
+  high_old_HO = get_byte_HO();
+
+  while (!(high_new_HO == high_old_HO)){
+    high_new_HO = get_byte_HO();
+    high_old_HO = get_byte_HO();
+    }
+  result_high_HO = high_new_HO ;
   return;
 }
 
 //function for getting LSB
-void get_low () {
-  low_new = get_byte();
-  low_old = get_byte();
+void get_low_BR () {
+  low_new_BR = get_byte_BR();
+  low_old_BR = get_byte_BR();
   
-  while (!(low_new == low_old)){
-    low_new = get_byte();
-    low_old = get_byte();
+  while (!(low_new_BR == low_old_BR)){
+    low_new_BR = get_byte_BR();
+    low_old_BR = get_byte_BR();
     }
-  result_low = low_new ;
+  result_low_BR = low_new_BR ;
   return;
 }
-
-//function for converting 8-bits to a byte
-byte get_byte (){
-  bitWrite(byte_data, 0, digitalRead(30));
-  bitWrite(byte_data, 1, digitalRead(31));
-  bitWrite(byte_data, 2, digitalRead(32));
-  bitWrite(byte_data, 3, digitalRead(33));
-  bitWrite(byte_data, 4, digitalRead(34));
-  bitWrite(byte_data, 5, digitalRead(35));
-  bitWrite(byte_data, 6, digitalRead(36));
-  bitWrite(byte_data, 7, digitalRead(37));   
-  return byte_data;     
+void get_low_HO () {
+  low_new_HO = get_byte_HO();
+  low_old_HO = get_byte_HO();
+  
+  while (!(low_new_HO == low_old_HO)){
+    low_new_HO = get_byte_HO();
+    low_old_HO = get_byte_HO();
+    }
+  result_low_HO = low_new_HO ;
+  return;
 }
-
+//function for converting 8-bits to a byte
+byte get_byte_BR (){
+  bitWrite(byte_data_BR, 0, digitalRead(30));
+  bitWrite(byte_data_BR, 1, digitalRead(31));
+  bitWrite(byte_data_BR, 2, digitalRead(32));
+  bitWrite(byte_data_BR, 3, digitalRead(33));
+  bitWrite(byte_data_BR, 4, digitalRead(34));
+  bitWrite(byte_data_BR, 5, digitalRead(35));
+  bitWrite(byte_data_BR, 6, digitalRead(36));
+  bitWrite(byte_data_BR, 7, digitalRead(37));   
+  return byte_data_BR;     
+}
+byte get_byte_HO (){
+  bitWrite(byte_data_HO, 0, digitalRead(40));
+  bitWrite(byte_data_HO, 1, digitalRead(41));
+  bitWrite(byte_data_HO, 2, digitalRead(42));
+  bitWrite(byte_data_HO, 3, digitalRead(43));
+  bitWrite(byte_data_HO, 4, digitalRead(44));
+  bitWrite(byte_data_HO, 5, digitalRead(45));
+  bitWrite(byte_data_HO, 6, digitalRead(46));
+  bitWrite(byte_data_HO, 7, digitalRead(47));   
+  return byte_data_HO;     
+}
 
--- reading_count_arduino/runMotor.ino
+++ reading_count_arduino/runMotor.ino
@@ -1,41 +1,98 @@
 void runMotor(){
+
+    //**********************HO HO HO*******************
   
-    //desired_location = 20*sin(40*millis()/1000.0);                                        // sine wave input -- just set whole thing to 80 for a step!
+    //desired_location_HO = 20*sin(40*millis()/1000.0);                                        // sine wave input -- just set whole thing to 80 for a step!
     
-    error = desired_location - count;
+    error_HO = desired_location_HO - count_HO;
 
     // *****   PID   *****
-        integral += error*((micros()-last_micros_integral)*0.000001);                      // accumulate ongoing error
-          last_micros_integral = micros();
-        if (derivative_counter == 60){             
-          derivative = (error - last_error)/(((micros()-last_micros_derivative)*0.000001));    // difference in current error minus the last error
-          last_micros_derivative = micros();
-          last_error = error;                                                                // update last error to current error
-          derivative_counter = 0;
+        integral_HO += error_HO*((micros()-last_micros_integral_HO)*0.000001);                      // accumulate ongoing error
+          last_micros_integral_HO = micros();
+        if (derivative_counter_HO == 20){             
+          derivative_HO = (error_HO - last_error_HO)/(((micros()-last_micros_derivative_HO)*0.000001));    // difference in current error minus the last error
+          last_micros_derivative_HO = micros();
+          last_error_HO = error_HO;                                                                // update last error to current error
+          derivative_counter_HO = 0;
         }
-        derivative_counter++;
-        PID_value = (Kp * error) + (Ki * integral) + (Kd * derivative);                    // updating variable "error" using PID values
+        derivative_counter_HO++;
+        PID_value_HO = (Kp_HO * error_HO) + (Ki_HO * integral_HO) + (Kd_HO * derivative_HO);                    // updating variable "error" using PID values
     // ***** END PID *****
     
-    error_decimal = abs(PID_value)/100;
-    raw_PWM =  error_decimal*226+29;                                                          // PWM is scaled by the error, maxing out at 100 slots
-    if (raw_PWM > 255)
-      raw_PWM = 255;
-
+    error_decimal_HO = abs(PID_value_HO)/100;
+    raw_PWM_HO =  error_decimal_HO*205+50;                                                          // PWM is scaled by the error, maxing out at 100 slots
+    if (raw_PWM_HO  > 255)
+      raw_PWM_HO = 255;
+      
       //Serial.print(millis());
       //Serial.print("\t");
-      //Serial.print(count);
-
-    analogWrite(ENABLE, raw_PWM);
+      //Serial.println(micros()-last_micros_integral_HO);
       
-    if (PID_value >= 0) {
-      digitalWrite(dir1, HIGH);
-      digitalWrite(dir2, LOW);
+      
+
+    analogWrite(ENABLE_HO, raw_PWM_HO);
+      
+    if (PID_value_HO >= 0) {
+      digitalWrite(dir1_HO, HIGH);
+      digitalWrite(dir2_HO, LOW);
     }
     else {
-      digitalWrite(dir1, LOW);
-      digitalWrite(dir2, HIGH);
+      digitalWrite(dir1_HO, LOW);
+      digitalWrite(dir2_HO, HIGH);
     }
+
+
+
+
+
+
+
+
+
+
+    
+
+
+
+    //**********************BRIAN*******************
+
+    //desired_location_BR = 20*cos(40*millis()/1000.0);                                        // sine wave input -- just set whole thing to 80 for a step!
+    
+    error_BR = desired_location_BR - count_BR;
+
+    // *****   PID   *****
+        integral_BR += error_BR*((micros()-last_micros_integral_BR)*0.000001);                      // accumulate ongoing error
+          last_micros_integral_BR = micros();
+        if (derivative_counter_BR == 20){             
+          derivative_BR = (error_BR - last_error_BR)/(((micros()-last_micros_derivative_BR)*0.000001));    // difference in current error minus the last error
+          last_micros_derivative_BR = micros();
+          last_error_BR = error_BR;                                                                // update last error to current error
+          derivative_counter_BR = 0;
+        }
+        derivative_counter_BR++;
+        PID_value_BR = (Kp_BR * error_BR) + (Ki_BR * integral_BR) + (Kd_BR * derivative_BR);                    // updating variable "error" using PID values
+    // ***** END PID *****
+    
+    error_decimal_BR = abs(PID_value_BR)/100;
+    raw_PWM_BR =  error_decimal_BR*225+30;                                                          // PWM is scaled by the error, maxing out at 100 slots
+    if (raw_PWM_BR  > 255)
+      raw_PWM_BR = 255;
+      
+      //Serial.print(millis());
+      //Serial.print("\t");
+      //Serial.print(count_BR);
+
+    analogWrite(ENABLE_BR, raw_PWM_BR);
+      
+    if (PID_value_BR >= 0) {
+      digitalWrite(dir1_BR, HIGH);
+      digitalWrite(dir2_BR, LOW);
+    }
+    else {
+      digitalWrite(dir1_BR, LOW);
+      digitalWrite(dir2_BR, HIGH);
+    }
+
 
     /*if(millis() > 10000)
       Serial.end();           // ends display for easy copying after 10 seconds*/
